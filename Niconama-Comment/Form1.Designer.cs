4e9c1cf251ad075237c786b7fa166aee82370f1f
iedred7584 <iedred7584@gmail.com>
2018-03-15 00:22:06 +0900

    1st Commit

---
+++ Niconama-Comment/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Niconama_Comment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(868, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "過去コメント取得";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.ID,
+            this.Time1,
+            this.Time2,
+            this.Comment});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(998, 509);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // No
+            // 
+            this.No.Frozen = true;
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            this.No.ReadOnly = true;
+            // 
+            // ID
+            // 
+            this.ID.Frozen = true;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Time1
+            // 
+            this.Time1.Frozen = true;
+            this.Time1.HeaderText = "時刻";
+            this.Time1.Name = "Time1";
+            this.Time1.ReadOnly = true;
+            // 
+            // Time2
+            // 
+            this.Time2.Frozen = true;
+            this.Time2.HeaderText = "経過時刻";
+            this.Time2.Name = "Time2";
+            this.Time2.ReadOnly = true;
+            // 
+            // Comment
+            // 
+            this.Comment.Frozen = true;
+            this.Comment.HeaderText = "コメント";
+            this.Comment.Name = "Comment";
+            this.Comment.ReadOnly = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(262, 511);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(506, 19);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(769, 509);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "コメント投稿";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(185, 509);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "接続";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(2, 511);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 19);
+            this.textBox2.TabIndex = 6;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 60000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 532);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Niconama Comment";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comment;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ Niconama-Comment/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Niconama_Comment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            typeof(DataGridView).GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(dataGridView1, true, null);
+            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[1].Width = 200;
+            dataGridView1.Columns[2].Width = 80;
+            dataGridView1.Columns[3].Width = 80;
+            dataGridView1.Columns[4].Width = 580;
+            foreach (DataGridViewColumn c in dataGridView1.Columns) c.SortMode = DataGridViewColumnSortMode.Automatic;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+        }
+
+        string lv = "", ticket = "", thread = "", address = "", port = "", open_time = "", start_time = "", when = "";
+        NetworkStream ns;
+
+        CookieContainer cookie_container = new CookieContainer();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Task.Run(() =>
+            {
+                try
+                {
+                    #region ログイン
+                    using (HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookie_container })
+                    using (HttpClient c = new HttpClient(handler) { BaseAddress = new Uri("https://secure.nicovideo.jp/secure/login?site=niconico") }) { HttpResponseMessage result = c.PostAsync("", new FormUrlEncodedContent(new Dictionary<string, string> { { "next_url", "" }, { "mail", "メルアド" }, { "password", "パスワード" } })).Result; }
+                    #endregion
+
+                    #region GetLv
+                    if (lv == "")
+                    {
+                        using (HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookie_container })
+                        using (HttpClient c = new HttpClient(handler) { BaseAddress = new Uri("URL (例: http://live.nicovideo.jp/watch/ch2598539)") })
+                        {
+                            HttpResponseMessage ss = c.GetAsync("").Result;
+                            lv = new Regex("<meta property=\"og:url\" content=\"http://live.nicovideo.jp/watch/(?<text>.*?)\"", RegexOptions.IgnoreCase | RegexOptions.Multiline).Match(ss.Content.ReadAsStringAsync().Result).Groups["text"].Value;
+                        }
+                    }
+                    #endregion
+
+                    #region GetPlayerStatus
+                    using (HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookie_container })
+                    using (HttpClient c = new HttpClient(handler) { BaseAddress = new Uri("http://live.nicovideo.jp/api/getplayerstatus?v=" + lv) })
+                    {
+                        HttpResponseMessage ss = c.GetAsync("").Result;
+                        string gps = ss.Content.ReadAsStringAsync().Result.Replace("\n", "");
+                        Match gps_match = new Regex(@"<addr>(.*?)</addr><port>(.*?)</port><thread>(.*?)</thread>", RegexOptions.None).Match(gps);
+                        address = gps_match.Groups[1].Value;
+                        port = gps_match.Groups[2].Value;
+                        thread = gps_match.Groups[3].Value;
+                        Match time_match = new Regex(@"<open_time>(.*?)</open_time><start_time>(.*?)</start_time>", RegexOptions.None).Match(gps);
+                        open_time = time_match.Groups[1].Value;
+                        start_time = time_match.Groups[2].Value;
+                    }
+                    #endregion
+
+                    string request = "<thread thread=\"" + thread + "\" version=\"20061206\" res_from=\"-1000\" /> ";
+                    TcpClient tcp = new TcpClient(address, int.Parse(port));
+                    ns = tcp.GetStream();
+                    byte[] sendBytes = Encoding.UTF8.GetBytes(request);
+                    sendBytes[sendBytes.Length - 1] = 0;
+                    ns.Write(sendBytes, 0, sendBytes.Length);
+
+                    int resSize = 0;
+                    string merge = "";
+                    while (ns.CanRead)
+                    {
+                        byte[] resBytes = new byte[500];
+                        resSize = ns.Read(resBytes, 0, resBytes.Length);
+                        if (resSize == 0) break;
+                        string message = Encoding.UTF8.GetString(resBytes);
+                        string[] elements = message.Split(new string[] { "\0" }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (string r in elements)
+                        {
+                            if (r.StartsWith("<thread")) { ticket = new Regex("ticket=\"(.*?)\"", RegexOptions.None).Match(r).Groups[1].Value; Console.WriteLine(r); }
+                            string receive = r;
+                            if (!receive.EndsWith("</chat>"))
+                            {
+                                merge += receive;
+                                continue;
+                            }
+                            if (!receive.StartsWith("<chat")) { receive = merge + receive; merge = ""; }
+                            else if (receive.EndsWith("</chat>")) { receive = merge + receive; merge = ""; }
+                            else if (merge != "") { receive = merge + receive; merge = ""; }
+                            if (receive.StartsWith("<chat") && receive.EndsWith("</chat>"))
+                            {
+                                string comment = new Regex(">(.*)<", RegexOptions.None).Match(receive).Groups[1].Value.Replace("&lt;", "<").Replace("&gt;", ">");
+                                if (comment != "")
+                                {
+                                    if (when == "") when = new Regex("date=\"(.*?)\"", RegexOptions.None).Match(receive).Groups[1].Value;
+                                    if (comment.Contains("/hb ifseetno")) continue;
+                                    double no = double.Parse(new Regex("no=\"(.*?)\"", RegexOptions.None).Match(receive).Groups[1].Value);
+                                    string vpos = DateTimeOffset.FromUnixTimeSeconds((long)(double.Parse(new Regex("date=\"(.*?)\"", RegexOptions.None).Match(receive).Groups[1].Value) - double.Parse(open_time))).ToString("HH:mm:ss");
+                                    string date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(new Regex("date=\"(.*?)\"", RegexOptions.None).Match(receive).Groups[1].Value)).LocalDateTime.ToString("HH:mm:ss");
+                                    string id = new Regex("user_id=\"(.*?)\"", RegexOptions.None).Match(receive).Groups[1].Value;
+
+                                    Invoke(new Action(() =>
+                                    {
+                                        dataGridView1.Rows.Add(no, id, date, vpos, comment);
+                                        try { dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.Rows.Count - 1; } catch { }
+                                        dataGridView1.Sort(dataGridView1.CurrentCell.OwningColumn, ListSortDirection.Ascending);
+                                    }));
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e /* 過去コメ */)
+        {
+            Task.Run(() =>
+            {
+                string waybackkey = "";
+                using (HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookie_container })
+                using (HttpClient c = new HttpClient(handler)) waybackkey = c.GetStringAsync("http://watch.live.nicovideo.jp/api/getwaybackkey?thread=" + thread).Result.Replace("waybackkey=", "");
+                byte[] sendBytes_ = Encoding.UTF8.GetBytes("<thread thread=\"" + thread + "\" version=\"20061206\" res_from=\"-1000\" waybackkey=\"" + waybackkey + "\"  user_id=\"64924091\" when=\"" + when + "\"/> ");
+                when = "";
+                sendBytes_[sendBytes_.Length - 1] = 0;
+                ns.Write(sendBytes_, 0, sendBytes_.Length);
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e /* 投稿 */)
+        {
+            Task.Run(() =>
+            {
+                long unixtime = (long)DateTime.Now.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds - (long)double.Parse(open_time);
+                using (HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookie_container })
+                using (HttpClient c = new HttpClient(handler))
+                {
+                    string postkey = c.GetStringAsync("http://ow.live.nicovideo.jp/api/getpostkey?thread=" + thread).Result.Replace("postkey=", "");
+                    byte[] sendBytes_ = Encoding.UTF8.GetBytes("<chat thread=\"" + thread + "\" ticket=\"" + ticket + "\" vpos=\"" + unixtime + "\" postkey=\"" + postkey + "\" mail=\"184\" user_id=\"" + "64924091" + "\" premium=\"\">" + textBox1.Text + "</chat> ");
+                    sendBytes_[sendBytes_.Length - 1] = 0;
+                    ns.Write(sendBytes_, 0, sendBytes_.Length);
+                }
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e /* 接続 */)
+        {
+            if (ns != null) ns.Close();
+            lv = textBox2.Text;
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e /* HeartBeat */)
+        {
+            using (HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookie_container })
+            using (HttpClient c = new HttpClient(handler))
+            {
+                HttpResponseMessage heartbeat = c.PostAsync("http://ow.live.nicovideo.jp/api/heartbeat", new FormUrlEncodedContent(new Dictionary<string, string> { { "v", lv } })).Result;
+                Console.WriteLine(heartbeat.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
